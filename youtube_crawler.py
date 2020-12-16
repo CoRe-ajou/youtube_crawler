d6b87b381dbe2fdb3673230fe8fce0c032fa5bc1
Yeongin Park <pyi7628@gmail.com>
2020-12-17 05:54:33 +0900

    환경변수 변경 & 댓글 가져오는 코드 추가

--- youtube_crawler.py
+++ youtube_crawler.py
@@ -10,7 +10,7 @@ from google.auth.transport.requests import Request
 from googleapiclient.discovery import build
 from googleapiclient.errors import HttpError
 
-CLIENT_SECRETS_FILE = "/Users/yeong-in/Desktop/pyi/코리/dev/client_secret_415059270452-s5jn59vb5v8cqb9go6juqspltbj41c0h.apps.googleusercontent.com.json" #delete file name, if you post this code later
+CLIENT_SECRETS_FILE=os.environ["YOUTUBE_V3_CLIENT_SECRET_FILE"]
 SCOPES = ['https://www.googleapis.com/auth/youtube.force-ssl']
 API_SERVICE_NAME = 'youtube'
 API_VERSION = 'v3'
@@ -18,9 +18,46 @@ API_VERSION = 'v3'
 
 #api를 사용할 수 있는 서비스를 빌드한다.
 def get_authenticated_service():
-    flow = InstalledAppFlow.from_client_secrets_file(CLIENT_SECRETS_FILE, SCOPES)
-    credentials = flow.run_console()
+    credentials = None
+    if os.path.exists('token.pickle'):
+        with open('token.pickle', 'rb') as token:
+            credentials = pickle.load(token)
+    #  Check if the credentials are invalid or do not exist
+    if not credentials or not credentials.valid:
+        # Check if the credentials have expired
+        if credentials and credentials.expired and credentials.refresh_token:
+            credentials.refresh(Request())
+        else:
+            flow = InstalledAppFlow.from_client_secrets_file(
+                CLIENT_SECRETS_FILE, SCOPES)
+            credentials = flow.run_console()
+ 
+        # Save the credentials for the next run
+        with open('token.pickle', 'wb') as token:
+            pickle.dump(credentials, token)
+ 
     return build(API_SERVICE_NAME, API_VERSION, credentials = credentials)
+
+
+
+def get_video_comments(service, **kwargs):
+    comments = []
+    results = service.commentThreads().list(**kwargs).execute()
+ 
+    while results:
+        for item in results['items']:
+            comment = item['snippet']['topLevelComment']['snippet']['textDisplay']
+            comments.append(comment)
+ 
+        # Check if another page exists
+        if 'nextPageToken' in results:
+            kwargs['pageToken'] = results['nextPageToken']
+            results = service.commentThreads().list(**kwargs).execute()
+        else:
+            break
+ 
+    return comments
+
 
 
 if __name__ == '__main__':
@@ -28,3 +65,7 @@ if __name__ == '__main__':
     # running in production *do not* leave this option enabled.
     os.environ['OAUTHLIB_INSECURE_TRANSPORT'] = '1'
     service = get_authenticated_service()
+    video_id = input('Enter a video ID: ')
+    get_video_comments(service, part='snippet', videoId=video_id, eventType='completed', textFormat='plainText')
+ 
+
